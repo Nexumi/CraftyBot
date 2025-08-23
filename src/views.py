ac6ae2f1dfc3616ab9104f02d2079d0f0e75fa1e
Jimmy Pan <jimmypan13570@gmail.com>
2025-08-23 02:42:54 -0600

    feat: add confirmation tracking to views

--- src/views.py
+++ src/views.py
@@ -2,8 +2,10 @@ import discord
 
 class RestartConfirmation(discord.ui.View):
   callback = None
+  confirmed = False
 
 
   @discord.ui.button(label="Restart", style=discord.ButtonStyle.primary, emoji="🔁")
   async def button_callback(self, button, interaction):
+    RestartConfirmation.confirmed = True
     await RestartConfirmation.callback(interaction.message)
