7306ca31fea6ce151fc5796369d47f473bb3d1bb
Jimmy Pan <jimmypan13570@gmail.com>
2026-03-03 21:44:43 -0700

    fix: do not log interaction from restart confirmation

--- src/views.py
+++ src/views.py
@@ -5,7 +5,7 @@ import discord
 class RestartConfirmation(discord.ui.View):
   @discord.ui.button(label="Restart", style=discord.ButtonStyle.primary, emoji="🔁")
   async def button_callback(self, button, interaction):
-    utils.log_request(interaction, {"interaction": interaction, "RestartConfirmation": True})
+    utils.log_request(interaction, {"RestartConfirmation": True})
     if await utils.is_valid_user(interaction, self.bot):
       self.confirmed = True
       await self.callback(interaction.message)
